301f081ac8b10e21e7d88631e51ba8ac9442107d
Florian <fcangrand@arca-computing.fr>
2022-02-24 18:53:51 +0100

    parse contributors

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ filename = fname.split("input/")[1]
 with open(fname) as f:
     lines = f.readlines()
 
-nb_clients = int(lines[0])
+line_0 = lines[0].split(" ")
+nb_contributors = int(line_0[0])
+nb_projects = int(line_0[1])
+
+print(nb_contributors)
+print(nb_projects)
 
 
+line_index = 1
+for i in range(nb_contributors):
+    name, nb_skills = lines[line_index].split(" ")
+    print(name)
+    line_index += 1
+    for j in range(int(nb_skills)):
+        skill, level = lines[line_index+j].split(" ")
+        print(skill)
+        print(int(level))
+    line_index += int(nb_skills)
 
 # Partie Ecriture Sortie
 fichier = open("output/" + filename, "w+")
